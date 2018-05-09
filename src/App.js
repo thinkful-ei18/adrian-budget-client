f68c64ccca7e74d6d3e9f0d42549eb4bde201e4b
Adrian <adriantoddross@gmail.com>
2018-05-08 21:36:21 -0400

    create root route

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route } from 'react-router-dom';
 import './App.css';
 
 import Navbar from './components/Navbar';
+import LandingPage from './components/LandingPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Navbar/>
+      <Route exact path="/" component={LandingPage} />
       </div>
     );
   }
