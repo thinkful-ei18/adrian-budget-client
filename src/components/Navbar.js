4c5a2433c8cb2914e092594b165cbc96e5ade580
Adrian <adriantoddross@gmail.com>
2018-05-09 02:26:30 -0400

    remove class names

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,15 @@ import './Navbar.css';
 export default class Navbar extends Component {
   render() {
     return(
-      <nav className='navbar'>
-        <h1 className='appName'>Windfall</h1>
-        <ul className='navlist'>
+     <div>
+      <nav>
+        <h1>Windfall</h1>
+        <ul>
           <li>Sign up</li>
           <li>Log in</li>
         </ul>
       </nav>
+    </div>
     );
   }
 }
