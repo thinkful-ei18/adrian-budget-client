59993022b1a10911e5dac2a9cd97dfc4d9b437ec
Adrian <adriantoddross@gmail.com>
2018-05-08 22:14:14 -0400

    stub header info

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import './LandingPage.css';
 
 export default class LandingPage extends Component {
   render() {
     return(
-      <div>
-
+      <div className='landingPage'>
+        <header className='header'>
+          <p>Header info about the app!</p>
+        </header>
       </div>
     );
   }
