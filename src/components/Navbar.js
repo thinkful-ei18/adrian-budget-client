fd207a094ae30aa99e89c319219a2ca6b31fa0a4
Adrian <adriantoddross@gmail.com>
2018-05-09 01:09:16 -0400

    indent elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,9 @@ export default class Navbar extends Component {
   render() {
     return(
       <nav className='navbar'>
-      <h1 className='appName'>Windfall</h1>
-      <button className="navButton">Sign Up</button>
-      <button className="navButton">Log in</button>
+        <h1 className='appName'>Windfall</h1>
+        <button className="navButton">Sign Up</button>
+        <button className="navButton">Log in</button>
       </nav>
     );
   }
