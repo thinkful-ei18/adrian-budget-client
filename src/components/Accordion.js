ad7af620d9399e74636ab7de4935b0e1dccacf8d
Adrian <adriantoddross@gmail.com>
2018-05-10 22:49:08 -0400

    use this.props

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
+import Bill from './Bill';
 
 export default class Accordion extends Component {
   render() {
     return (
-
+      <div className='accordion'>
+        <div className='title'>
+          {this.props.title}
+        </div>
+          <Bill name='Spotify' amount='15'/>
+          <Bill name='Charter' amount='65.99'/>
+          <Bill name='Utilities' amount='250'/>
+      </div>
     );
   }
 }
