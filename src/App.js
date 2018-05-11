51847bbb6712dd7e2e03ecb4f4b3ee608ce0bb62
Adrian <adriantoddross@gmail.com>
2018-05-11 01:10:14 -0400

    add 404 route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 
 import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
 import Footer from './components/Footer';
 import Dashboard from './components/Dashboard';
+import NotFoundPage from './components/404';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Navbar/>
+      <Switch>
       <Route exact path="/" component={LandingPage}/>
       <Route path='/dashboard' component={Dashboard}/>
+      <Route component={NotFoundPage} />
+      </Switch>
       <Footer/>
       </div>
     );
