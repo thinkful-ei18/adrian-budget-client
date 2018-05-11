58355f67777646369ad84f0ea0fa52a5e5ff07e0
Adrian <adriantoddross@gmail.com>
2018-05-10 21:05:53 -0400

    add route & path for dashboard

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import './App.css';
 import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
 import Footer from './components/Footer';
+import Dashboard from './components/Dashboard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Navbar/>
-      <Route exact path="/" component={LandingPage} />
+      <Route exact path="/" component={LandingPage}/>
+      <Route path='/dashboard' component={Dashboard}/>
       <Footer/>
       </div>
     );
