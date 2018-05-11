f271895b5b1a3fbd15ae6013002849796d12387d
Adrian <adriantoddross@gmail.com>
2018-05-11 00:57:15 -0400

    create 404 component

---
+++ src/components/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function NotFoundPage(props) {
+  return (
+    <div>
+      <main>
+      <h1>Sorry!</h1>
+      <p>We can't find the page you're looking for.</p>
+      <button>Go back home!</button>
+      </main>
+    </div>
+  );
+}
