8595fd85f14743dcc2e18b2022d45acbbccd08a2
Adrian <adriantoddross@gmail.com>
2018-05-11 00:57:27 -0400

    indent jsx

--- src/components/404.js
+++ src/components/404.js
@@ -4,9 +4,9 @@ export default function NotFoundPage(props) {
   return (
     <div>
       <main>
-      <h1>Sorry!</h1>
-      <p>We can't find the page you're looking for.</p>
-      <button>Go back home!</button>
+        <h1>Sorry!</h1>
+        <p>We can't find the page you're looking for.</p>
+        <button>Go back home!</button>
       </main>
     </div>
   );
