8101ed2fa60ee2356e5b6a366f877acbf989ae5f
Adrian <adriantoddross@gmail.com>
2018-05-10 20:15:19 -0400

    add html

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import {React} from 'react';
+
+export default function Footer(props){
+  return(
+    <div>
+      <footer>
+        <p>Created by Adrian Ross</p>
+        <nav>
+          <ul>
+            <li><a href="http://">Client Repo</a></li>
+            <li><a href="http://">Server Repo</a></li>
+          </ul>
+        </nav>
+      </footer>
+    </div>
+  );
+}
