6bb536870a4c6b655932118ca565bdf7592a176f
Adrian <adriantoddross@gmail.com>
2018-05-11 21:57:20 -0400

    create list component based on react docs

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Bill from './Bill';
 
-export default function List(props){
+export default function List(props) {
+
+  const bills = [
+    {id: 1, name: 'Spotify', amount: 15},
+    {id: 2, name: 'Netflix', amount: 11},
+    {id: 3, name: 'Charter', amount: 65.99}
+  ];
+
+  const listItems = bills.map((item) =>
+    <li key={item.id}>
+      <Bill name={item.name} amount={item.amount}/>
+    </li>
+  );
+
   return (
     <div>
-
+      <ul>
+        {listItems}
+      </ul>
     </div>
   );
 }
