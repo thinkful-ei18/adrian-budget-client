8e0e5b92054c2f6f1e5fb48afda303ff699164c6
Adrian <adriantoddross@gmail.com>
2018-05-11 23:05:48 -0400

    import loginform

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import LandingPage from './components/LandingPage';
 import Footer from './components/Footer';
 import Dashboard from './components/Dashboard';
 import RegisterForm from './components/RegistrationForm';
+import LoginForm from './components/LoginForm';
 import NotFoundPage from './components/404';
 
 class App extends Component {
@@ -18,6 +19,7 @@ class App extends Component {
           <Route exact path="/" component={LandingPage}/>
           <Route path='/dashboard' component={Dashboard}/>
           <Route path='/register' component={RegisterForm}/>
+          <Route path='/login' component={LoginForm}/>
           <Route component={NotFoundPage} />
         </Switch>
         <Footer/>
