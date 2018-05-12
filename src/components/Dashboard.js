fac64eab5446e6ff137e0d8b3f7e59e2eefbc741
Adrian <adriantoddross@gmail.com>
2018-05-11 21:45:49 -0400

    remove accordion

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import Accordion from './Accordion';
 
 export default class Dashboard extends Component {
   render() {
@@ -7,7 +6,6 @@ export default class Dashboard extends Component {
       <div>
           <button>Hamburger Icon</button>
         <div>
-          <Accordion/>
           <nav>
             <ul>
               <li>New Bill</li>
