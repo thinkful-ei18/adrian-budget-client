064f966378c07154fda2d30d61717acbd3b18400
Adrian <adriantoddross@gmail.com>
2018-05-13 12:52:45 -0400

    render form conditionally

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -1,20 +1,32 @@
 import React, {Component, Fragment} from 'react';
+import { Field, reduxForm, focus } from "redux-form";
+import Input from "./Input";
 import './Bill.css';
 
-export default class Bill extends Component {
+export class Bill extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      expanded: false
+      expanded: false,
+      editing: false,
     };
 
     this.togglePanel = this.togglePanel.bind(this);
     this.deleteBill = this.deleteBill.bind(this);
+    this.toggleEditing = this.toggleEditing.bind(this);
   }
 
   togglePanel() {
     this.setState({expanded: !this.state.expanded});
+  }
+
+  handleFormSubmit(values) {
+    console.log(values);
+  }
+
+  toggleEditing() {
+    this.setState({editing: !this.state.editing})
   }
 
   deleteBill(id) {
@@ -22,9 +34,94 @@ export default class Bill extends Component {
   }
 
 render() {
-  const { id, name, amount, duedate, beenpaid, billinterval, category_id } = this.props;
+  const { pristine, submitting, handleSubmit, id, title, amount, duedate, beenpaid, interval, category_id } = this.props;
 
   const collapsibleIdentifier = `collapsible-${id}`;
+
+  let info;
+
+  if (this.state.editing) {
+    info =
+    <div
+      id={collapsibleIdentifier}
+      className='collapsible'
+      aria-hidden={!this.state.expanded}
+    >
+      <form
+        onSubmit={handleSubmit((values) => this.handleFormSubmit(values))}
+      >
+        <Field
+          label="Title"
+          component={Input}
+          type="text"
+          name="title"
+          value={title}
+        />
+        <Field
+          label="Amount"
+          component={Input}
+          type="number"
+          name="amount"
+          value={amount}
+        />
+        <Field
+          label="Due Date"
+          component={Input}
+          type="date"
+          name="date"
+          value={duedate}
+        />
+        <Field
+          label="Interval"
+          component='select'
+          name="interval"
+          value={interval}
+        >
+          <option></option>
+          <option value="P [ years-months-days ]">Weekly</option>
+          <option value="P [ years-months-days ]">Bi-weekly</option>
+          <option value="P [ years-months-days ]">Monthly</option>
+        </Field>
+        <button
+          onClick={() => this.toggleEditing(id)}
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          disabled={pristine || submitting}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  } else {
+    info =
+    <div
+      id={collapsibleIdentifier}
+      className='collapsible'
+      aria-hidden={!this.state.expanded}
+    >
+      <h1>{title}</h1>
+      <p>${amount}</p>
+      <p>
+        {duedate}
+        {beenpaid}
+        {interval}
+        {category_id}
+      </p>
+      <button
+        onClick={() => this.toggleEditing(id)}
+      >
+        Edit
+      </button>
+      <button
+        onClick={() => this.deleteBill(id)}
+      >
+        Delete
+      </button>
+    </div>
+  }
 
   return (
       <Fragment>
@@ -34,7 +131,7 @@ render() {
           aria-controls={collapsibleIdentifier}
           onClick={this.togglePanel}
         >
-          <h1>{name}</h1>
+          <h1>{title}</h1>
           <p>${amount}</p>
         </button>
           <div
@@ -42,14 +139,19 @@ render() {
             className='collapsible'
             aria-hidden={!this.state.expanded}
           >
-            <h1>{name}</h1>
+            <h1>{title}</h1>
             <p>${amount}</p>
             <p>
               {duedate}
               {beenpaid}
-              {billinterval}
+              {interval}
               {category_id}
             </p>
+            <button
+              onClick={() => this.toggleEditing(id)}
+            >
+              Edit
+            </button>
             <button
               onClick={() => this.deleteBill(id)}
             >
@@ -61,13 +163,17 @@ render() {
   }
 };
 
-
 Bill.defaultProps = {
   expanded: false,
   name: 'Happiness',
   amount: 55,
   duedate: '2018-03-20',
   beenpaid: false,
-  billinterval: 'P [ years-months-days ]',
+  interval: 'P [ years-months-days ]',
   category_id: [100, 101, 102]
 }
+
+export default reduxForm({
+	form: "Bill",
+	onSubmitFail: (errors, dispatch) => dispatch(focus("bill", Object.keys(errors)[0]))
+})(Bill)
