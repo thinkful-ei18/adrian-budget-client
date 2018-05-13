806a68adf0f6bc70990ba90eea416515c93856a6
Adrian <adriantoddross@gmail.com>
2018-05-13 12:17:29 -0400

    change name to title

--- src/components/List.js
+++ src/components/List.js
@@ -4,14 +4,14 @@ import Bill from './Bill';
 export default function List(props) {
 
   const bills = [
-    {id: 1, name: 'Spotify', amount: 15},
-    {id: 2, name: 'Netflix', amount: 11},
-    {id: 3, name: 'Charter', amount: 65.99}
+    {id: 1, title: 'Spotify', amount: 15},
+    {id: 2, title: 'Netflix', amount: 11},
+    {id: 3, title: 'Charter', amount: 65.99}
   ];
 
   const listItems = bills.map((item) =>
     <li key={item.id}>
-      <Bill id={item.id} name={item.name} amount={item.amount}/>
+      <Bill id={item.id} title={item.title} amount={item.amount}/>
     </li>
   );
 
