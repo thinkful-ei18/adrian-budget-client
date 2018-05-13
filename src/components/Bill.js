0aa4980d27ab12d18f17e71507fe9f8436f01215
Adrian <adriantoddross@gmail.com>
2018-05-13 13:26:09 -0400

    remove bill form

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -15,6 +15,7 @@ export class Bill extends Component {
     this.togglePanel = this.togglePanel.bind(this);
     this.deleteBill = this.deleteBill.bind(this);
     this.toggleEditing = this.toggleEditing.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
   togglePanel() {
@@ -34,70 +35,27 @@ export class Bill extends Component {
   }
 
 render() {
-  const { pristine, submitting, handleSubmit, id, title, amount, duedate, beenpaid, interval, category_id } = this.props;
+  const { id, title, amount, duedate, beenpaid, interval, category_id } = this.props;
 
   const collapsibleIdentifier = `collapsible-${id}`;
 
-  let info;
-
-  if (this.state.editing) {
-    info =
-    <div
-      id={collapsibleIdentifier}
-      className='collapsible'
-      aria-hidden={!this.state.expanded}
-    >
-      <form
-        onSubmit={handleSubmit((values) => this.handleFormSubmit(values))}
-      >
-        <Field
-          label="Title"
-          component={Input}
-          type="text"
-          name="title"
-          value={title}
-        />
-        <Field
-          label="Amount"
-          component={Input}
-          type="number"
-          name="amount"
-          value={amount}
-        />
-        <Field
-          label="Due Date"
-          component={Input}
-          type="date"
-          name="date"
-          value={duedate}
-        />
-        <Field
-          label="Interval"
-          component='select'
-          name="interval"
-          value={interval}
-        >
-          <option></option>
-          <option value="P [ years-months-days ]">Weekly</option>
-          <option value="P [ years-months-days ]">Bi-weekly</option>
-          <option value="P [ years-months-days ]">Monthly</option>
-        </Field>
+  return (
+      <Fragment>
         <button
-          onClick={() => this.toggleEditing(id)}
+          className='bill-toggle-btn'
+          aria-expanded={this.state.expanded}
+          aria-controls={collapsibleIdentifier}
+          onClick={this.togglePanel}
         >
-          Cancel
+          <h1>{title}</h1>
+          <p>${amount}</p>
         </button>
-        <button
-          type="submit"
-          disabled={pristine || submitting}
+        <div
+          id={collapsibleIdentifier}
+          className='collapsible'
+          aria-hidden={!this.state.expanded}
         >
-          Submit
-        </button>
-      </form>
-    </div>
-  } else {
-    info =
-    <div
+          <div
       id={collapsibleIdentifier}
       className='collapsible'
       aria-hidden={!this.state.expanded}
@@ -121,43 +79,7 @@ render() {
         Delete
       </button>
     </div>
-  }
-
-  return (
-      <Fragment>
-        <button
-          className='bill-toggle-btn'
-          aria-expanded={this.state.expanded}
-          aria-controls={collapsibleIdentifier}
-          onClick={this.togglePanel}
-        >
-          <h1>{title}</h1>
-          <p>${amount}</p>
-        </button>
-          <div
-            id={collapsibleIdentifier}
-            className='collapsible'
-            aria-hidden={!this.state.expanded}
-          >
-            <h1>{title}</h1>
-            <p>${amount}</p>
-            <p>
-              {duedate}
-              {beenpaid}
-              {interval}
-              {category_id}
-            </p>
-            <button
-              onClick={() => this.toggleEditing(id)}
-            >
-              Edit
-            </button>
-            <button
-              onClick={() => this.deleteBill(id)}
-            >
-              Delete
-            </button>
-          </div>
+        </div>
       </Fragment>
     );
   }
@@ -175,5 +97,5 @@ Bill.defaultProps = {
 
 export default reduxForm({
 	form: "Bill",
-	onSubmitFail: (errors, dispatch) => dispatch(focus("bill", Object.keys(errors)[0]))
+	onSubmitFail: (errors, dispatch) => dispatch(focus("Bill", Object.keys(errors)[0]))
 })(Bill)
