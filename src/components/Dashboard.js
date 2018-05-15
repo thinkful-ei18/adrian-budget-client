844a0ace361e0e054b44d2558ab1aa2c4d55f1cf
Adrian <adriantoddross@gmail.com>
2018-05-15 00:41:47 -0400

    render user class before <ul> options

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import List from './List';
+import User from './User';
 
 export default class Dashboard extends Component {
   render() {
     return (
       <div>
         <main>
+          <User/>
           <ul>
             <li>New Bill</li>
             <li>Change Income</li>
