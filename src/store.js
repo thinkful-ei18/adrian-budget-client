dad32be2a6bb58e4f73f9ed0542e5426dac6b6ce
Adrian <adriantoddross@gmail.com>
2018-05-18 17:14:10 -0400

    add user reducer

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import userReducer from './reducers/user-reducer';
 
 const store = createStore(
   combineReducers({
-    form: formReducer
+    form: formReducer,
+    currentUser: userReducer
   }),
   composeWithDevTools(applyMiddleware(thunk))
 );
