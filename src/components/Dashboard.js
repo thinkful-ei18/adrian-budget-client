ba815fcd0ea657c1e286cd241b85ab0d3db6fb3e
Adrian <adriantoddross@gmail.com>
2018-05-18 19:57:06 -0400

    toggle billForm with button

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,43 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import BillForm from '../components/BillForm';
 import List from './List';
 import User from './User';
+import './Dashboard.css';
 
 export class Dashboard extends Component {
+
+  toggleBillForm() {
+    this.setState({editingBillForm: !this.state.editingBillForm});
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showBillForm: false,
+      editingBillForm: false,
+      showIncomeForm: false
+    }
+
+    this.toggleBillForm = this.toggleBillForm.bind(this);
+  }
+
   render() {
-    const { loggedIn, user } = this.props;
+    const { id, loggedIn, user } = this.props;
+    let billForm;
+    let incomeForm;
+    const billFormIdentifier = `billForm-${id}`;
 
     if (!loggedIn) {
       return <Redirect to='/'/>;
+    }
+
+    if (this.state.editingBillForm) {
+      billForm = <BillForm editButton={this.toggleBillForm}/>
+    } else {
+      billForm = '';
     }
 
     return (
@@ -17,10 +45,26 @@ export class Dashboard extends Component {
         <main>
           <User firstname={user.firstname} grossincome={user.income}/>
           <ul>
-            <li>New Bill</li>
+            <li>
+              <button
+                className='billform-toggle-btn'
+                aria-expanded={this.state.showBillForm}
+                aria-controls={billFormIdentifier}
+                onClick={this.toggleBillForm}
+              >
+                New Bill
+              </button>
+              </li>
             <li>Change Income</li>
             <li>Logout</li>
           </ul>
+          <div
+            id={billFormIdentifier}
+            className='bill-form-collapsible'
+            aria-hidden={!this.state.showBillForm}
+          >
+            {billForm}
+          </div>
           <List/>
         </main>
       </div>
