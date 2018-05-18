1bee9bc786f6a07be9ab690851654a59cacc709f
Adrian <adriantoddross@gmail.com>
2018-05-18 17:25:50 -0400

    remove whitespace

--- src/actions/user-actions.js
+++ src/actions/user-actions.js
@@ -79,8 +79,6 @@ export const login = (username, password) => dispatch => {
         password,
       }),
     })
-      // Reject any requests which don't return a 200 status, creating
-      // errors which follow a consistent format
       .then(res => normalizeResponseErrors(res))
       .then(res => res.json())
       // .then(({ authToken }) => storeAuthToken(authToken, dispatch))
