6531269d1001d3f8b4f660d185b9d86fa1a2394a
Adrian <adriantoddross@gmail.com>
2018-05-19 01:27:31 -0400

    stub fetchBills action

--- src/actions/bills-actions.js
+++ src/actions/bills-actions.js
@@ -1,3 +1,6 @@
+import { normalizeResponseErrors } from '../utils/normalize-errors';
+import { loadAuthToken } from '../local-storage';
+
 export const FETCH_BILLS_REQUEST = 'FETCH_BILLS_REQUEST';
 export const fetchBillsRequest = () => ({
   type: FETCH_BILLS_REQUEST,
@@ -13,3 +16,19 @@ export const addBillRequest = list => ({
   type: FETCH_BILLS_SUCCESS,
   list
 });
+
+export const fetchBills = () => dispatch => {
+  const authToken = localStorage.getItem('authToken');
+
+  return (
+    fetch(`${API_BASE_URL}/bills`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${authToken}`
+      },
+    })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => console.log('response:', res))
+  );
+};
