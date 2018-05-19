06753ceb050c3a3c8cd8c89c3e5b71c249c94b20
Adrian <adriantoddross@gmail.com>
2018-05-19 01:19:19 -0400

    handle actions for fetching bills

--- src/reducers/bills-reducer.js
+++ src/reducers/bills-reducer.js
@@ -1,16 +1,21 @@
-// import {} from '';
+import { FETCH_BILLS_REQUEST, FETCH_BILLS_SUCCESS, FETCH_BILLS_ERROR} from '../actions/bills-actions';
 
 const initialState = {
-  bills: null,
+  list: null,
   loading: false,
   error: null,
 }
 
 export const billsReducer = (state=initialState, action) => {
   switch (action.type) {
-    case value:
+    case FETCH_BILLS_REQUEST:
+      return Object.assign({}, state, {loading: true});
 
-      break;
+    case FETCH_BILLS_ERROR:
+      return Object.assign({}, state, {loading: false, error: action.error});
+
+      case FETCH_BILLS_SUCCESS:
+      return Object.assign({}, state, {loading: false, list: action.list});
 
     default:
       return state;
