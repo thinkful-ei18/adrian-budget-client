2b7db04ea4156f6016a9f4ce9abda549a7e2c942
Adrian <adriantoddross@gmail.com>
2018-05-19 01:08:16 -0400

    stub bills reducer

---
+++ src/reducers/bills-reducer.js
@@ -0,0 +1,18 @@
+// import {} from '';
+
+const initialState = {
+  bills: null,
+  loading: false,
+  error: null,
+}
+
+export const billsReducer = (state=initialState, action) => {
+  switch (action.type) {
+    case value:
+
+      break;
+
+    default:
+      return state;
+  }
+}
