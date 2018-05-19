9adf18c6febbf72defe7d0e4746b1549349d2570
Adrian <adriantoddross@gmail.com>
2018-05-19 01:40:03 -0400

    connect list and mapStatetoProps

--- src/components/List.js
+++ src/components/List.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import {Bill} from './Bill';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Bill } from './Bill';
+import { fetchBills } from '../actions/bills-actions';
 
-export default function List(props) {
+export class List extends Component {
 
-  const bills = [
-    {id: 1, title: 'Spotify', amount: 15},
-    {id: 2, title: 'Netflix', amount: 11},
-    {id: 3, title: 'Charter', amount: 65.99}
-  ];
+  componentDidMount() {
+    this.props.dispatch(fetchBills());
+  }
 
-  const listItems = bills.map((item) =>
-    <li key={item.id}>
-      <Bill id={item.id} title={item.title} amount={item.amount}/>
-    </li>
-  );
+  render() {
 
-  return (
-    <div>
-      <ul>
-        {listItems}
-      </ul>
-    </div>
-  );
-}
+    const bills = [
+      {id: 1, title: 'Spotify', amount: 15},
+      {id: 2, title: 'Netflix', amount: 11},
+      {id: 3, title: 'Charter', amount: 65.99}
+    ];
+
+    const listItems = bills.map((item) =>
+      <li key={item.id}>
+        <Bill id={item.id} title={item.title} amount={item.amount}/>
+      </li>
+    );
+
+    return (
+      <div>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    );
+  }
+};
+
+export const mapStateToProps = (state, props) => ({
+  bills: state.bills.list
+});
+
+export default connect(mapStateToProps)(List);
