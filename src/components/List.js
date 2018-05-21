cc643d1adfbf84bc9af435479dba8d1a562ca9de
Adrian <adriantoddross@gmail.com>
2018-05-21 12:36:10 -0400

    Add instructions for creating your first bill

--- src/components/List.js
+++ src/components/List.js
@@ -11,18 +11,27 @@ export class List extends Component {
   }
 
   render() {
+    let bills;
+    let listItems;
 
-    const bills = [
-      {id: 1, title: 'Spotify', amount: 15},
-      {id: 2, title: 'Netflix', amount: 11},
-      {id: 3, title: 'Charter', amount: 65.99}
-    ];
+    // bills = [
+    //   {id: 1, title: 'Spotify', amount: 15},
+    //   {id: 2, title: 'Netflix', amount: 11},
+    //   {id: 3, title: 'Charter', amount: 65.99}
+    // ];
 
-    const listItems = bills.map((item) =>
-      <li key={item.id}>
-        <Bill id={item.id} title={item.title} amount={item.amount}/>
-      </li>
-    );
+    if (this.props.userBills) {
+      bills = this.props.userBills;
+
+      listItems = bills.map((item) =>
+        <li key={item.id}>
+          <Bill id={item.id} title={item.title} amount={item.amount}/>
+        </li>
+      );
+    } else {
+      listItems =
+        <li>Let's add your first bill! Click 'New Bill', add its' details, then it'll appear in this list!</li>
+    }
 
     return (
       <div>
@@ -36,7 +45,7 @@ export class List extends Component {
 
 export const mapStateToProps = (state, props) => ({
   hasAuthToken: state.currentUser.authToken !== null,
-  bills: state.bills.list
+  userBills: state.bills.list
 });
 
 export default connect(mapStateToProps)(List);
