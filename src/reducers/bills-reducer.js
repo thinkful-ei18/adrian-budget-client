85b273c02251f65f846941a776f1c3f8fb277e50
Adrian <adriantoddross@gmail.com>
2018-05-21 11:14:10 -0400

    handle create bill actions

--- src/reducers/bills-reducer.js
+++ src/reducers/bills-reducer.js
@@ -1,4 +1,4 @@
-import { FETCH_BILLS_REQUEST, FETCH_BILLS_SUCCESS, FETCH_BILLS_ERROR} from '../actions/bills-actions';
+import { FETCH_BILLS_REQUEST, FETCH_BILLS_SUCCESS, FETCH_BILLS_ERROR, CREATE_BILL_REQUEST, CREATE_BILL_ERROR, CREATE_BILL_SUCCESS} from '../actions/bills-actions';
 
 const initialState = {
   list: null,
@@ -14,8 +14,17 @@ export const billsReducer = (state=initialState, action) => {
     case FETCH_BILLS_ERROR:
       return Object.assign({}, state, {loading: false, error: action.error});
 
-      case FETCH_BILLS_SUCCESS:
+    case FETCH_BILLS_SUCCESS:
       return Object.assign({}, state, {loading: false, list: action.list});
+
+    case CREATE_BILL_REQUEST:
+    return Object.assign({}, state, {loading: true});
+
+    case CREATE_BILL_ERROR:
+    return Object.assign({}, state, {loading: false, error: action.error});
+
+    case CREATE_BILL_SUCCESS:
+    return Object.assign({}, state, {loading: false});
 
     default:
       return state;
