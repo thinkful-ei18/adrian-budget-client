45453f5ce83223a3edcdd56f3604ef17b0318699
Adrian <adriantoddross@gmail.com>
2018-05-22 15:26:29 -0400

    handle delete request and error

--- src/reducers/bills-reducer.js
+++ src/reducers/bills-reducer.js
@@ -1,4 +1,4 @@
-import { FETCH_BILLS_REQUEST, FETCH_BILLS_SUCCESS, FETCH_BILLS_ERROR, CREATE_BILL_REQUEST, CREATE_BILL_ERROR, CREATE_BILL_SUCCESS, CLEAR_BILLS} from '../actions/bills-actions';
+import { FETCH_BILLS_REQUEST, FETCH_BILLS_SUCCESS, FETCH_BILLS_ERROR, CREATE_BILL_REQUEST, CREATE_BILL_ERROR, CREATE_BILL_SUCCESS, CLEAR_BILLS, DELETE_BILL_REQUEST, DELETE_BILL_ERROR, DELETE_BILL_SUCCESS} from '../actions/bills-actions';
 
 const initialState = {
   list: null,
@@ -18,16 +18,25 @@ export const billsReducer = (state=initialState, action) => {
       return Object.assign({}, state, {loading: false, list: action.list});
 
     case CREATE_BILL_REQUEST:
-    return Object.assign({}, state, {loading: true});
+      return Object.assign({}, state, {loading: true});
 
     case CREATE_BILL_ERROR:
-    return Object.assign({}, state, {loading: false, error: action.error});
+      return Object.assign({}, state, {loading: false, error: action.error});
 
     case CREATE_BILL_SUCCESS:
-    return {...state, list: [...state.list, action.bill]};
+      return {...state, list: [...state.list, action.bill]};
 
     case CLEAR_BILLS:
-    return Object.assign({}, state, {list: null, loading: false, error: null});
+      return Object.assign({}, state, {list: null, loading: false, error: null});
+
+    case DELETE_BILL_REQUEST:
+      return Object.assign({}, state, {loading: true});
+
+    case DELETE_BILL_ERROR:
+      return Object.assign({}, state, {loading: false, error: action.error});
+
+    // case DELETE_BILL_SUCCESS:
+    //   return {...state, list: [...state.list, action.bill]};
 
     default:
       return state;
