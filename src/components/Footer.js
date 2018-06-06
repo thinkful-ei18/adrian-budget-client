5539dde17d06967c2b5663031701ddd5496a93d8
Adrian <adriantoddross@gmail.com>
2018-06-06 18:17:49 -0400

    finalize dashboard & footer CSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,8 +11,9 @@ export default function Footer(props) {
         <p>
           Designed and developed by <a className='attribution' href="https://www.adriantoddross.com/" target="_blank" rel="noopener noreferrer">Adrian Ross</a>.
         </p>
-        <p>Hosted on Netlify. Code is licensed under MIT and available on Github. All images and graphics are licensed under CC BY-SA. © 2018 Adrian Ross
+        <p>Hosted on Netlify. Code is licensed under MIT and available on Github. All images and graphics are licensed under CC BY-SA.
         </p>
+        <p>© 2018 Adrian Ross</p>
         <nav className='footer-nav'>
           <ul className='footer-list'>
             <li><a href="https://github.com/thinkful-ei18/adrian-budget-client" target="_blank" rel="noopener noreferrer">Client Repo</a></li>
