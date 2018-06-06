4ad51664ed2c4fb0d3130ff1bfd635630a217ad9
Adrian <adriantoddross@gmail.com>
2018-06-06 16:21:39 -0400

    add css for user

--- src/components/User.js
+++ src/components/User.js
@@ -4,10 +4,10 @@ import './User.css';
 export default function User(props) {
   return (
     <div className='user'>
-      <h1 className='user-firstname'>Hello {props.firstname}!</h1>
+      <h1 className='user-firstname'>Hello, {props.firstname}!</h1>
       <h2 className='user-grossincome'>You make ${props.grossincome} a month.</h2>
       <p className='user-netincome'>
-        After bills, you'll have: ${props.netincome} left over.
+        After bills, you'll have ${props.netincome} left over.
       </p>
     </div>
   );
