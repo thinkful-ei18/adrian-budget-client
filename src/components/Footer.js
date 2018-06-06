e4518208b515d69a0589f713f3ee9721fd697112
Adrian <adriantoddross@gmail.com>
2018-06-06 16:47:36 -0400

    add footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import './Footer.css';
 
 export default function Footer(props) {
   return(
     <div>
-      <footer>
-        <p>Created by Adrian Ross</p>
-        <nav>
-          <ul>
-            <li><a href="http://">Client Repo</a></li>
-            <li><a href="http://">Server Repo</a></li>
+      <footer className='footer'>
+        <div className='footer-img-container'>
+          <img className='footer-img' src="https://res.cloudinary.com/adriantoddross/image/upload/v1527785141/adrian-ross.png" alt="Adrian Ross"/>
+        </div>
+        <p>
+          Designed and developed by <a className='attribution' href="https://www.adriantoddross.com/" target="_blank" rel="noopener noreferrer">Adrian Ross</a>.
+        </p>
+        <p>Hosted on Netlify. Code is licensed under MIT and available on Github. All images and graphics are licensed under CC BY-SA. © 2018 Adrian Ross
+        </p>
+        <nav className='footer-nav'>
+          <ul className='footer-list'>
+            <li><a href="https://github.com/thinkful-ei18/adrian-budget-client" target="_blank" rel="noopener noreferrer">Client Repo</a></li>
+            <li><a href="https://github.com/thinkful-ei18/adrian-budget-server" target="_blank" rel="noopener noreferrer">Server Repo</a></li>
           </ul>
         </nav>
       </footer>
